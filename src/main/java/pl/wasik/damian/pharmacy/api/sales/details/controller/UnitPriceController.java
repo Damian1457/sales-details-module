6d741c12ddeb75a1f8cbd00f9be8563adb9a6f0e
Damian1457 <damianwasik.coach@gmail.com>
2023-05-09 19:44:53 +0200

    Added swagger.

---
+++ src/main/java/pl/wasik/damian/pharmacy/api/sales/details/controller/UnitPriceController.java
@@ -0,0 +1,40 @@
+package pl.wasik.damian.pharmacy.api.sales.details.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.wasik.damian.pharmacy.api.sales.details.model.UnitPrice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/unitprices")
+public class UnitPriceController {
+
+    @GetMapping
+    public List<UnitPrice> list() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public UnitPrice create(@RequestBody UnitPrice unitPrice) {
+        return new UnitPrice();
+    }
+
+    @GetMapping(value = "/{id}")
+    public UnitPrice read(@PathVariable Long id) {
+        return new UnitPrice();
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+}
